281b72f9c0493ee4dd92dfde1fd2aeb640ced52e
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2023-10-14 13:33:59 +0330

    updated 1402/7/22 change color btn header

--- src/components/modules/ButtonHeader.jsx
+++ src/components/modules/ButtonHeader.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 const ButtonHeader = ({icon , color , name , count}) => {
   return (
     <div className='flex flex-col  items-center justify-between text-sm min-w-[80px]'>
-        <div className='w-[50px] h-[50px]  rounded-full flex justify-center items-center ' style={{background : color}}>
+        <div className='w-[50px] h-[50px]  rounded-full flex justify-center items-center  ' style={{background : color}}>
             {icon}
         </div>
-        <p className='mt-2'>
+        <p className='mt-2 text-gray-dark'>
             {count}
         </p>
-        <p className='text-center'>
+        <p className='text-center text-gray-dark'>
             {name}
         </p>
     </div>
